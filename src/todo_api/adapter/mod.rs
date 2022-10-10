2df9d7363e5b6b887ca38a0e1784fc9296fca4ed
belagoesr <belagoesr@gmail.com>
2022-10-09 23:30:06 -0500

    remove serde-dynamo lib

--- src/todo_api/adapter/mod.rs
+++ src/todo_api/adapter/mod.rs
@@ -1,3 +1,10 @@
+use actix_web::web;
+use uuid::Uuid;
+
+use crate::todo_api_web::model::todo::{TodoCard, State};
+
+use super::model::{TodoCardDb, TaskDb, StateDb};
+
 #[macro_export]
 macro_rules! val {
     (B => $bval:expr) => {{
@@ -12,4 +19,27 @@ macro_rules! val {
     (M => $val:expr) => {{
         AttributeValue::M($val)
     }};
+}
+
+
+pub fn todo_json_to_db(card: web::Json<TodoCard>) -> TodoCardDb {
+    TodoCardDb {
+        id: Uuid::new_v4(),
+        title: card.title.clone(),
+        description: card.description.clone(),
+        owner: card.owner,
+        tasks: card
+            .tasks
+            .iter()
+            .map(|t| TaskDb {
+                is_done: t.is_done,
+                title: t.title.clone(),
+            })
+            .collect(),
+        state: match card.state {
+            State::Todo => StateDb::Todo,
+            State::Doing => StateDb::Doing,
+            State::Done => StateDb::Done,
+        },
+    }
 }
--- src/todo_api_web/controller/todo.rs
+++ src/todo_api_web/controller/todo.rs
@@ -1,16 +1,19 @@
-use crate::todo_api::db::todo::create_todo as put_todo;
+use crate::todo_api::adapter;
+use crate::todo_api::db:: {helpers::get_client, todo::put_todo};
 use crate::todo_api_web::model::todo::{TodoCard, TodoIdResponse};
 use actix_web::{http::header::ContentType, post, web, HttpResponse, Responder};
-use uuid::Uuid;
 
 #[post("/api/create")]
-pub async fn create_todo(payload: web::Json<TodoCard>) -> impl Responder {
-    let new_id = Uuid::new_v4();
-    let str = serde_json::to_string(&TodoIdResponse::new(new_id));
-
-    put_todo(payload).await;
-
-    HttpResponse::Created()
+pub async fn create_todo(info: web::Json<TodoCard>) -> impl Responder {
+    let todo_card = adapter::todo_json_to_db(info);
+    let client = get_client().await;
+    match put_todo(&client, todo_card).await {
+        None => HttpResponse::BadRequest().body("Failed to create todo card"),
+        Some(id) => HttpResponse::Created()
         .content_type(ContentType::json())
-        .body(str.expect("failed to serialize ContactsBatchResponseId"))
+            .body(
+                serde_json::to_string(&TodoIdResponse::new(id))
+                    .expect("Failed to serialize todo card"),
+            ),
+    }
 }
