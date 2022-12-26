fde1fd702837d175695f856e9230d8bd34c0ed04
belagoesr <belagoesr@gmail.com>
2022-12-25 18:25:55 -0600

    docker wip - issue with connecting to db from web container + include Logs

--- src/main.rs
+++ src/main.rs
@@ -5,13 +5,12 @@ use todo_server::{todo_api::db::helpers::create_table, todo_api_web::routes::app
 
 use actix_web::middleware::Logger;
 use actix_web::{App, HttpServer};
-//use bastion::prelude::*;
 use env_logger;
 
 #[actix_web::main]
 async fn main() -> Result<(), std::io::Error> {
     std::env::set_var("RUST_LOG", "actix_web=info");
-    //env_logger::init();
+    env_logger::init();
     create_table().await;
     HttpServer::new(|| {
         App::new()
@@ -27,15 +26,3 @@ async fn main() -> Result<(), std::io::Error> {
     .run()
     .await
 }
-
-// #[fort::root(redundancy = 10)]
-// async fn main(_: BastionContext) -> Result<(), ()> {
-//     std::env::set_var("RUST_LOG", "actix_web=info");
-//     env_logger::init();
-//     create_table().await;
-
-//     let _ = web_main();
-
-//     // TODO: still throwing error since init() is called more then 1 time
-//     Ok(())
-// }
